69e4461da132150a11ffca4344728c97c39fde01
Fábio Senra <fabio-senra@hotmail.com>
2019-10-25 13:47:03 +0100

    TP6 falta acabar PR

---
+++ TP6/arqson/public/javascripts/update.js
@@ -0,0 +1,14 @@
+function updateItem (prov,local,tit,musico){
+    console.log('ola')
+	console.log('vou tentar atualizar o' + prov +'...')
+	axios.put('/',{
+        uprov:prov,
+        ulocal:local,
+        utit:tit,
+        umusico:musico
+    })
+	.then(function(response){
+            window.location.assign('/')
+    })
+	.catch(error=>console.log(prov +"ola"))
+}
